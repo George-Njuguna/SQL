dc109baba5962658f26a38b65ce40c8bbb6ab952
George-Njuguna <mailtomegeorge0@gmail.com>
2026-01-21 13:22:57 +0300

    .

--- Schema_n_normalization.sql
+++ Schema_n_normalization.sql
@@ -6,7 +6,7 @@ CREATE TABLE student_sports(
     student_id INTEGER NOT NULL,
     /* Sport played*/
     sport VARCHAR (200) NOT NULL
-)
+);
 
 /*
 Loading the data in 1NF Format
@@ -31,7 +31,7 @@ CREATE  TABLE student_sports(
     student_name VARCHAR(200) NOT NULL,
     sport VARCHAR(200) NOT NULL,
     coach VARCHAR(200) NOT NULL
-)
+);
 /* From the above table the table is 1NF to change it to 2NF 
  - student_id does not depend on entry_id (PK) but depends on the student_id therfore we can remove the part that depends on students id 
  - coach doesnt depend on the entry id but depends on the sport we will also remove it and place it in a new table 
@@ -40,17 +40,17 @@ CREATE TABLE student_sports(
     entry_id SERIAL PRIMARY KEY,
     student_id INTEGER NOT NULL,
     sport VARCHAR(200) NOT NULL
-)
+);
 
 CREATE TABLE students(
     student_id INTEGER NOT NULL PRIMARY KEY,
     student_name VARCHAR(200) NOT NULL
-)
+);
 
 CREATE TABLE sports(
     sport VARCHAR(200) NOT NULL PRIMARY KEY,
     coach VARCHAR(200) NOT NULL
-)
+);
 /* Now student_id and sport become FOREIGN KEYS that connect their respective tables to the student_sports table 
  Achieving 2NF
 */
