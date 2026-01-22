c30f5d73d9fa31b921e2b590c334d35622788863
George-Njuguna <mailtomegeorge0@gmail.com>
2026-01-22 15:26:17 +0300

    .

--- Schema_n_normalization.sql
+++ Schema_n_normalization.sql
@@ -43,7 +43,7 @@ when a table has a composite key here is when we check for 2NF
 CREATE TABLE student_sports(
     student_id INTEGER NOT NULL,
     sport VARCHAR(200) NOT NULL
-    PRIMARY KEY(student_id, sport) 
+    PRIMARY KEY(student_id, sport) /* creating a composite key */
 );
 CREATE TABLE students(
     student_id INTEGER NOT NULL PRIMARY KEY,
