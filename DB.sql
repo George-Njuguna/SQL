dc573cc347b886a8d90d95c08b45f729c0572ad9
George-Njuguna <mailtomegeorge0@gmail.com>
2026-01-26 11:26:44 +0300

    .

--- DB.sql
+++ DB.sql
@@ -12,3 +12,11 @@ CREATE TABLE IF NOT EXISTS projects_lead(
     project_name VARCHAR(100) NOT NULL,
     client_name VARCHAR(100) NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS consultants(
+    consultant_name VARCHAR(100) NOT NULL,
+    consultant_email VARCHAR(100) NOT NULL,
+    hourly_rate INTEGER NOT NULL,
+    PRIMARY KEY(consultant_name, consultant_email)
+);
+
