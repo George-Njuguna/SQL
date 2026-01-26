aa874fd540742fa47769487106e79bc0ae7718db
George-Njuguna <mailtomegeorge0@gmail.com>
2026-01-26 11:58:19 +0300

    build tables

--- DB.sql
+++ DB.sql
@@ -7,22 +7,26 @@ CREATE DATABASE IF NOT EXISTS wrhsetestdb;
 /*
     creating tables that are 3NF
 */
-CREATE TABLE IF NOT EXISTS projects_lead(
-    lead_id SERIAL PRIMARY KEY,
-    consultant_name VARCHAR(100) NOT NULL,
-    project_name VARCHAR(100) NOT NULL,
-    client_name VARCHAR(100) NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS consultants(
+    consultant_id SERIAL PRIMARY KEY,
     consultant_name VARCHAR(100) NOT NULL,
     consultant_email VARCHAR(100) NOT NULL,
-    hourly_rate INTEGER NOT NULL,
-    PRIMARY KEY(consultant_name, consultant_email)
+    hourly_rate INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS clients(
-    client_name VARCHAR(100) NOT NULL PRIMARY KEY,
+    client_id SERIAL PRIMARY KEY,
+    client_name VARCHAR(100) NOT NULL,
     client_hq_city VARCHAR(100) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS projects(
+    project_id SERIAL PRIMARY KEY,
+    project_name VARCHAR(100) NOT NULL,
+    consultant_id INTEGER REFERENCES consultants(consultant_id),
+    client_id INTEGER REFERENCES clients(client_id)
+);
+
+/*
+    Creating indexes
+*/
