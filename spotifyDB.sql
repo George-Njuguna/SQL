56d325224e905a3287b5b5def40e8bea2932b5f0
George-Njuguna <mailtomegeorge0@gmail.com>
2026-02-24 14:48:54 +0300

    Created spotify_wrhse

--- spotifyDB.sql
+++ spotifyDB.sql
@@ -1,1 +1,20 @@
 -- This project is where i try to rebuild my existing spotify database using what i have learnt 
+
+CREATE DATABASE spotify_wrhse;
+
+/* 
+    In my databse there are different tables nameley :
+                 followed_artists       
+                 genre                  
+                 playlist_tracks        
+                 playlists              
+                 recently_played_tracks 
+                 saved_albums           
+                 saved_tracks           
+                 top_artists            
+                 top_tracks 
+
+    Where these tables do not follow the DataBase Design Principles.
+    I will therefore start with recently_played_tracks
+
+    
