f4067c85d029d6d1b3281e42eaa977527b00152e
George-Njuguna <mailtomegeorge0@gmail.com>
2026-02-24 15:41:21 +0300

    .

--- spotifyDB.sql
+++ spotifyDB.sql
@@ -21,8 +21,8 @@ CREATE DATABASE spotify_wrhse;
 /*                  recently_played_tracks
     when we check the table structure of this table we get
                     Table "public.recently_played_tracks"
-        Column    |           Type           | Collation | Nullable | Default
-        -------------+--------------------------+-----------+----------+---------
+        Column      |           Type           | Collation | Nullable | Default
+        ------------+--------------------------+-----------+----------+---------
         played_at   | timestamp with time zone |           | not null |
         id          | text                     |           |          |
         name        | text                     |           | not null |
@@ -37,5 +37,12 @@ CREATE DATABASE spotify_wrhse;
             "recently_played_tracks_pkey" PRIMARY KEY, btree (played_at)
     
     NORMAL FORMS
-    The table is not in 1NF since the primary Key (played at) can be divided into two data timestamp and timezone therfore we need
-    to change it to only show the time 
+    The table is already 1NF since all the columns have atomic content(indivisible) 
+
+    2NF : This is to remove redundancy in the table but since the table has 1 primary key the table already is in 2NF 
+          BUT we can change the primary Key to a number which is faster in lookups and Joins therefore "entry_id will be BIGSERIAL PRIMARY KEY"
+
+    3NF : The table is not 3NF since some columns do not soley depend on the primary key directly therfore that part of the table needs to be taken to another table
+          album_name directly depend on the album_id and not the primary key therefore we will create an albumns_table 
+          artist_name directly depend on the artist_id and not the primary key therefore we will create an artists_table 
+          we will also remove the explicit column since it doesnt give new information.
