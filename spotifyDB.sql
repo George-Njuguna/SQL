780f0e8d1333417646d1e52963ec47c07f040357
George-Njuguna <mailtomegeorge0@gmail.com>
2026-02-24 15:05:35 +0300

    .

--- spotifyDB.sql
+++ spotifyDB.sql
@@ -16,5 +16,26 @@ CREATE DATABASE spotify_wrhse;
 
     Where these tables do not follow the DataBase Design Principles.
     I will therefore start with recently_played_tracks
+*/
 
+/*                  recently_played_tracks
+    when we check the table structure of this table we get
+                    Table "public.recently_played_tracks"
+        Column    |           Type           | Collation | Nullable | Default
+        -------------+--------------------------+-----------+----------+---------
+        played_at   | timestamp with time zone |           | not null |
+        id          | text                     |           |          |
+        name        | text                     |           | not null |
+        artist_name | text                     |           | not null |
+        artist_id   | text                     |           |          |
+        album_name  | text                     |           | not null |
+        album_id    | text                     |           |          |
+        duration    | integer                  |           |          |
+        explicit    | boolean                  |           |          | true
+        popularity  | integer                  |           |          |
+        Indexes:
+            "recently_played_tracks_pkey" PRIMARY KEY, btree (played_at)
     
+    NORMAL FORMS
+    The table is not in 1NF since the primary Key (played at) can be divided into two data timestamp and timezone therfore we need
+    to change it to only show the time 
